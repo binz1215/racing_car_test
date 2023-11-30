1cc3d93e030f815047cc70046913b5ac9c6c520f
binz1215 <binz1215@naver.com>
2023-11-30 14:25:41 +0900

    이름 유효성 검사 클래스 수정

--- src/model/validate_car.js
+++ src/model/validate_car.js
@@ -18,13 +18,16 @@ class ValidateCar {
   }
 
   #validateLength() {
-    return this.#carName.every((name) => name.length >= MIN_LENGTH && name.length <= MAX_LENGTH);
+    if (!this.#carName.every((name) => name.length >= MIN_LENGTH && name.length <= MAX_LENGTH)) {
+      throw Error('[ERROR] 1~5사이의 길이만 입력 가능합니다.');
+    }
   }
 
   #validateDuple() {
     const nameSet = new Set(this.#carName);
-    if (this.#carName.length !== nameSet.size) return false;
-    return true;
+    if (this.#carName.length !== nameSet.size) {
+      throw Error('[ERROR] 중복된 이름은 입력할 수 없습니다.');
+    }
   }
 
   getNames() {
