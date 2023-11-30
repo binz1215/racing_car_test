ebfc220c1a502ab734406c569582f6c9c20dc55c
binz1215 <binz1215@naver.com>
2023-11-30 14:19:59 +0900

    숫자 유효성 검사 클래스 작성

--- src/model/validate_num.js
+++ src/model/validate_num.js
@@ -1,6 +1,7 @@
 import { carArray, countNum } from '../controller/make-array';
 
-class Validate {
+class ValidateNum {
+  static MIN_NUM = 1;
   #count;
 
   constructor(counts) {
@@ -9,10 +10,32 @@ class Validate {
     this.#validate();
   }
 
-  #validate() {}
+  #validate() {
+    this.#validateIsNum();
+    this.#validatePositive();
+    this.#validateIsInteger();
+  }
+
+  #validateIsNum() {
+    if (Number.isNaN(this.#count)) {
+      return false;
+    }
+    return true;
+  }
+
+  #validatePositive() {
+    if (this.#count < MIN_NUM) {
+      return false;
+    }
+    return true;
+  }
+
+  #validateIsInteger() {
+    return Number.isInteger(this.#count);
+  }
 
   getCounts() {
     return this.#count;
   }
 }
-export default Validate;
+export default ValidateNum;
