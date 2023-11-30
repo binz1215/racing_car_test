477ebdb34ab0a235eecec2db9dbd3bccb8d57949
binz1215 <binz1215@naver.com>
2023-11-30 13:31:56 +0900

    input

---
+++ src/view/input.js
@@ -0,0 +1,14 @@
+import { Console } from '@woowacourse/mission-utils';
+
+async function getName() {
+  const carName = await Console.readLineAsync('경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n');
+
+  return carName;
+}
+async function getCount() {
+  const count = await Console.readLineAsync('시도할 횟수는 몇 회인가요?\n');
+
+  return count;
+}
+
+export { getName, getCount };
