8a5427b19db809ea4af7901d4db10b4734adfd1f
binz1215 <binz1215@naver.com>
2023-12-01 12:19:25 +0900

    empty

--- src/App.js
+++ src/App.js
@@ -1,33 +1,5 @@
-import { carArray, countNum } from './controller/make-array.js';
-import GoOrNot from './model/go_or_not.js';
-import ValidateCar from './model/validate_car.js';
-import ValidateNum from './model/validate_num.js';
-import { Console } from '@woowacourse/mission-utils';
-import { winner } from './controller/find_winner.js';
-
 class App {
-  async play() {
-    const names = await carArray();
-    const validCar = new ValidateCar(names);
-    const validName = validCar.getNames();
-
-    const rawCount = await countNum();
-    const numCount = new ValidateNum(rawCount);
-    const validNum = numCount.getCounts();
-    Console.print('\n');
-
-    Console.print('실행결과\n');
-
-    const goOrNot = new GoOrNot(validName, validNum);
-
-    const oneSet = goOrNot.oneSet();
-
-    const win = winner(oneSet);
-    Console.print('최종 우승자 : ' + win);
-  }
+  async play() {}
 }
 
 export default App;
-
-const app = new App();
-app.play();
--- src/view/output.js
+++
@@ -1,9 +0,0 @@
-import { Console } from '@woowacourse/mission-utils';
-
-function outputPrint(set) {
-  Object.entries(set).forEach(([key, value]) => {
-    Console.print(`${key} : ${value}`);
-  });
-}
-
-export { outputPrint };
